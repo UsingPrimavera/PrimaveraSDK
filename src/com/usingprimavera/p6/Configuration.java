397056586d55c4b46afd32da6f2f404c9d4dc250
Barrie Callender <barrie@callenb.org>
2019-04-21 23:07:25 +0100

    read Configuration file

---
+++ src/com/usingprimavera/p6/Configuration.java
@@ -0,0 +1,118 @@
+package com.usingprimavera.p6;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+
+import java.util.Properties;
+
+public class Configuration {
+
+  private static String DEFAULT_CONFIGURATION_FILENAME = System.getProperty("user.dir") + File.separator + "PrimaveraSDK.properties";
+
+  private Properties _props = null;
+  private File _file = null;
+
+  public Configuration() {
+    this(DEFAULT_CONFIGURATION_FILENAME);
+  }
+
+  public Configuration(String filePathName) {
+
+    _props = new Properties(createDefaultConfiguration());
+
+    try {
+      _file = createOrReturnFile(filePathName);
+	    FileInputStream in = new FileInputStream(_file);
+	    _props.load(in);
+	    in.close();
+    }
+    catch (java.io.IOException ex) {
+      System.out.println("IOException reading configuration file:" + ex);
+    }
+
+
+  }
+
+  public String getFileName() {
+    return _file.getName();
+  }
+
+  public String getPathName() {
+    return _file.getPath();
+  }
+
+  public String getP6SessionType() {
+    return _props.getProperty("P6SessionType", null);
+  }
+
+  public String getP6RMIHostName() {
+    return _props.getProperty("P6RMIHostName", null);
+  }
+
+  public long getP6RMIPort() {
+    return Long.parseLong(_props.getProperty("P6RMIPort", null));
+  }
+
+  public Boolean getP6BatchFailOnFirstFlag() {
+    return Boolean.parseBoolean(_props.getProperty("P6BatchFailOnFirstFlag", null));
+  }
+
+  public long getP6BusinessObjectBatchSize() {
+    return Long.parseLong(_props.getProperty("P6BusinessObjectBatchSize", null));
+  }
+
+  public long getP6BindParameterLimitOnLoad() {
+    return Long.parseLong(_props.getProperty("P6BindParameterLimitOnLoad", null));
+  }
+
+  public String getP6WSHostName() {
+    return _props.getProperty("P6WSHostName", null);
+  }
+
+  public String getP6WSSecurity() {
+    return _props.getProperty("P6WSSecurity", null);
+  }
+
+  /**
+   * Create default Properties
+   **/
+  private Properties createDefaultConfiguration() {
+
+    Properties defaultProps = new Properties();
+
+    defaultProps.setProperty("P6SessionType","LOCAL_SERVICE");
+    defaultProps.setProperty("P6RMIHostName","http://ple-1808-demo");
+    defaultProps.setProperty("P6RMIPort","9099");
+    defaultProps.setProperty("P6BatchFailOnFirstFlag","true");
+    defaultProps.setProperty("P6BusinessObjectBatchSize","1000");
+    defaultProps.setProperty("P6BindParameterLimitOnLoad","20");
+    defaultProps.setProperty("P6WSHostName","http://ple-1808-demo");
+    defaultProps.setProperty("P6WSSecurity","UsernameToken");
+
+    return defaultProps;
+  }
+
+  /**
+   * create a new configuration file or return an existing one
+   **/
+  private File createOrReturnFile(String filename) {
+
+    File file = null;
+    boolean isCreated = false;
+
+    try {
+      file = new File(filename);
+      if (file.exists()) return file;
+      isCreated = file.createNewFile();
+    }
+    catch (IOException ex) {
+      System.out.println("createOrReturnFile() Error while creating a new empty file :" + ex);
+    }
+
+    return file;
+  }
+
+
+}
---
+++ test/com/usingprimavera/p6/ConfigurationTest.java
@@ -0,0 +1,121 @@
+package com.usingprimavera.p6;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ConfigurationTest {
+
+
+	@Test
+	public void testLoadDefaultConfigurationFile() {
+    String filename = "PrimaveraSDK.properties";
+    createConfigurationFile(filename,createDefaultConfiguration());
+    Configuration config = new Configuration();
+		assertEquals(filename, config.getFileName());
+    assertEquals(getDefaultPathname() + filename, config.getPathName());
+    assertEquals("LOCAL_SERVICE", config.getP6SessionType());
+    assertEquals("http://ple-1808-demo", config.getP6RMIHostName());
+    assertEquals(9099, config.getP6RMIPort());
+    assertEquals(true, config.getP6BatchFailOnFirstFlag());
+    assertEquals(1000, config.getP6BusinessObjectBatchSize());
+    assertEquals(20, config.getP6BindParameterLimitOnLoad());
+    assertEquals("http://ple-1808-demo", config.getP6WSHostName());
+    assertEquals("UsernameToken", config.getP6WSSecurity());
+	}
+
+  @Test
+	public void testLoadSpecifiedConfigurationFile() {
+    String filename = "MyOwnPrimaveraSDK.properties";
+    createConfigurationFile(filename,createCustomConfiguration());
+    Configuration config = new Configuration(getDefaultPathname() + filename);
+		assertEquals(filename, config.getFileName());
+    assertEquals(getDefaultPathname() + filename, config.getPathName());
+    assertEquals("STANDARD_RMI_SERVICE", config.getP6SessionType());
+    assertEquals("http://ple-1808-custom", config.getP6RMIHostName());
+    assertEquals(9024, config.getP6RMIPort());
+    assertEquals(false, config.getP6BatchFailOnFirstFlag());
+    assertEquals(2000, config.getP6BusinessObjectBatchSize());
+    assertEquals(10, config.getP6BindParameterLimitOnLoad());
+    assertEquals("http://ple-1808-ws", config.getP6WSHostName());
+    assertEquals("SAML", config.getP6WSSecurity());
+
+	}
+
+  /**
+   * Create default Properties
+   **/
+  private Properties createDefaultConfiguration() {
+
+    Properties defaultProps = new Properties();
+
+    defaultProps.setProperty("P6SessionType","LOCAL_SERVICE");
+    defaultProps.setProperty("P6RMIHostName","http://ple-1808-demo");
+    defaultProps.setProperty("P6RMIPort","9099");
+    defaultProps.setProperty("P6BatchFailOnFirstFlag","true");
+    defaultProps.setProperty("P6BusinessObjectBatchSize","1000");
+    defaultProps.setProperty("P6BindParameterLimitOnLoad","20");
+    defaultProps.setProperty("P6WSHostName","http://ple-1808-demo");
+    defaultProps.setProperty("P6WSSecurity","UsernameToken");
+
+    return defaultProps;
+  }
+
+   /**
+    * Create a custom Configuration file
+    **/
+    private Properties createCustomConfiguration() {
+
+      Properties defaultProps = new Properties();
+
+      defaultProps.setProperty("P6SessionType","STANDARD_RMI_SERVICE");
+      defaultProps.setProperty("P6RMIHostName","http://ple-1808-custom");
+      defaultProps.setProperty("P6RMIPort","9024");
+      defaultProps.setProperty("P6BatchFailOnFirstFlag","false");
+      defaultProps.setProperty("P6BusinessObjectBatchSize","2000");
+      defaultProps.setProperty("P6BindParameterLimitOnLoad","10");
+      defaultProps.setProperty("P6WSHostName","http://ple-1808-ws");
+      defaultProps.setProperty("P6WSSecurity","SAML");
+
+      return defaultProps;
+    }
+  /**
+   * Creates a Configuration file using given set of properties.  This will overwrite any
+   * existing Configuration file withthe same name
+   **/
+  private void createConfigurationFile(String filePathName, Properties configProps) {
+
+    File file = new File(filePathName);
+
+    try {
+      if (file.exists()) {
+        file.delete();
+        file.createNewFile();
+      }
+
+      FileOutputStream out = new FileOutputStream(file);
+      configProps.store(out, "---Testing---");
+      out.close();
+    }
+    catch (FileNotFoundException ex) {
+      System.out.println("FileNotFoundException writing " + filePathName);
+    }
+    catch (IOException ex) {
+      System.out.println("IOException writing " + filePathName);
+    }
+
+  }
+
+  /**
+   * returns the default path where the file is stored
+   **/
+  private String getDefaultPathname() {
+    return System.getProperty("user.dir") + File.separator;
+  }
+}
